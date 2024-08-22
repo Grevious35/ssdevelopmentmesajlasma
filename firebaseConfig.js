2859cbddf598b3eed1fe70d92d4de339084a8151
Grevious35 <112347517+Grevious35@users.noreply.github.com>
2024-08-22 12:55:57 +0300

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+// firebaseConfig.js
+  const firebaseConfig = {
+  apiKey: "AIzaSyDsidnJL_yKNo4-G5V1gNT6ZrrIEWX6FrA",
+  authDomain: "mesajlasma-71440.firebaseapp.com",
+  databaseURL:"https://mesajlasma-71440-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "mesajlasma-71440",
+  storageBucket: "mesajlasma-71440.appspot.com",
+  messagingSenderId: "287145584882",
+  appId: "1:287145584882:web:2e0d85c043e9433d40c86b",
+  measurementId: "G-KC5ZC2HJDW"
+};
+
+// Firebase'i başlatın
+firebase.initializeApp(firebaseConfig);
